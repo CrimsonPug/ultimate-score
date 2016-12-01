f4c4a12bf195422aa0d97be4564198bcb1efa7f6
CrimsonPug <ikram.tuan.mustapha@gmail.com>
2016-12-01 17:15:21 -0500

    asda

--- api/migrations/20161128132113_open-matches.js
+++ api/migrations/20161128132113_open-matches.js
@@ -1,6 +1,6 @@
 
 exports.up = function(knex, Promise) {
-  return knex.schema.createTableIfNotExists('open_matches', function (table) {
+  return knex.schema.createTableIfNotExists('open-matches', function (table) {
         table.increments('id').primary(); // adds incrementing int for id
         table.timestamp('created_at').notNullable().defaultTo(knex.fn.now());
         table.timestamp('updated_at').notNullable().defaultTo(knex.fn.now());
