e1c22ba3e8372d96537a0f0c70ad3775592dad2c
CrimsonPug <ikram.tuan.mustapha@gmail.com>
2016-12-01 17:09:58 -0500

    sad

--- api/migrations/20161128135430_open_teams.js
+++ api/migrations/20161128135430_open_teams.js
@@ -4,9 +4,7 @@ exports.up = function(knex, Promise) {
         table.increments('id').primary(); // adds incrementing int for id
         table.timestamp('created_at').notNullable().defaultTo(knex.fn.now());
         table.timestamp('updated_at').notNullable().defaultTo(knex.fn.now());
-        table.string('team_name') // adds a string column
-            .unique() // which has to be unique
-            .notNullable(); // and is required
+        table.string('team_name').unique().notNullable();
         table.integer('initial_seed').unique().notNullable();
         table.integer('games_played');
         table.integer('wins');
