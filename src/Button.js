22aec93128cfe3e334bd5a38499b14f79da69914
CrimsonPug <ikram.tuan.mustapha@gmail.com>
2016-12-03 15:22:53 -0500

    consol log

--- src/Button.js
+++ src/Button.js
@@ -14,6 +14,7 @@ class Score extends Component {
         this.props.handleAwayScore()
     }
     render(){
+        console.log(this.props.gameOver)
         if (this.props.gameOver === true){
             return(
                 <div className="Done">
