bac0e1a0173b2f1c300a18bd99640122469ca168
CrimsonPug <ikram.tuan.mustapha@gmail.com>
2016-12-04 23:25:35 -0500

    create match page

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import {Router, Route, IndexRoute, browserHistory} from 'react-router';
 import {Nav,Register,Login,PrivatePage} from './App';
 import {Home} from './Home';
 import {Match} from './Match';
+import {CreateMatch} from './CreateMatch';
 import './index.css';
 
 ReactDOM.render(
@@ -15,6 +15,7 @@ ReactDOM.render(
             <Route path="/login" component={Login} />
             <Route path="/match" component={Match} />
             <Route path="/private" component={PrivatePage} />
+            <Route path="/createMatch" component={CreateMatch} />
         </Route>
     </Router>,
   document.getElementById('root')
