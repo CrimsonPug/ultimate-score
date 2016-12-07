4eca725e1a3025d9fbeb2c06d9ee41306d04a9b9
CrimsonPug <ikram.tuan.mustapha@gmail.com>
2016-12-06 19:42:06 -0500

    asd

--- api/seeds/open_teams.js
+++ api/seeds/open_teams.js
@@ -5,8 +5,8 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       return Promise.all([
         // Inserts seed entries
-        knex('open_teams').insert({id: 10000000, team_name: 'BrainStation',abbr:'BSt',team_logo:'https://pbs.twimg.com/profile_images/658310539861479424/RRT0w90e.png'}),
-        knex('open_teams').insert({id: 20000000, team_name: 'Alberta FC',abbr:'AFC',team_logo:'https://pbs.twimg.com/profile_images/558112868865155072/NMa8K0Tn_400x400.jpeg'}),
+        knex('open_teams').insert({id: 1, team_name: 'BrainStation',abbr:'BSt',team_logo:'https://pbs.twimg.com/profile_images/658310539861479424/RRT0w90e.png'}),
+        knex('open_teams').insert({id: 2, team_name: 'Alberta FC',abbr:'AFC',team_logo:'https://pbs.twimg.com/profile_images/558112868865155072/NMa8K0Tn_400x400.jpeg'}),
       ]);
     });
 };
