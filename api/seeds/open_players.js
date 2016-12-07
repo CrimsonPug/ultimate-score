f055af3170d9435b3983ca1ca775badd0d4ed2e7
CrimsonPug <ikram.tuan.mustapha@gmail.com>
2016-12-06 19:31:50 -0500

    sadd

--- api/seeds/open_players.js
+++ api/seeds/open_players.js
@@ -5,7 +5,7 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       return Promise.all([
         // Inserts seed entries
-        knex('open_players').insert({id: 1, name: 'Alain Chicoine',squad_number:20,open_team_id:2,goals:0,assist:0}),
+        knex('open_players').insert({id: 200000, name: 'Alain Chicoine',squad_number:20,open_team_id:2,goals:0,assist:0}),
         // knex('open_players').insert({id: 20000, name: 'Will Crane',squad_number:23,open_team_id:1,goals:0,assist:0}),
       ]);
     });
