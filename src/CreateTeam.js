631b12fff836cfd350464b7a5e3a24c3c232444e
CrimsonPug <ikram.tuan.mustapha@gmail.com>
2016-12-06 21:21:17 -0500

    add team

--- src/CreateTeam.js
+++ src/CreateTeam.js
@@ -1,16 +1,58 @@
 import React, { Component } from 'react';
-import {Modal,Button,Tooltip,OverlayTrigger} from 'react-bootstrap';
+import {Modal,Button} from 'react-bootstrap';
+import axios from 'axios';
 import './create.css';
 
 class CreateTeam extends Component{
     constructor(){
         super();
-
+        this.state =({
+            team_name:null,
+            team_logo:null,
+            abbr:null,
+            showModal:false,
+        })
+        super();
+        this.txtFieldChange = this.txtFieldChange.bind(this)
+        this.close = this.close.bind(this)
+        this.open = this.open.bind(this)
+        this.saveTeam = this.saveTeam.bind(this)
+    }
+    saveTeam(){
+        this.setState({ 
+                showModal: false 
+            })
+        axios.post('/createTeam/addTeam',this.state)
+        .then((res)=>{
+            console.log(res.data)
+            location.href ="/private"
+        })
+    }
+    close() {
+        this.setState({ 
+                showModal: false 
+            })
+    }
+    open() {
+        this.setState({ 
+                showModal: true 
+            });
+    }
+    txtFieldChange(e){
+        if(e.target.name === "teamName"){
+            this.state.team_name = e.target.value
+        }else if(e.target.name === "logoURL"){
+            this.state.team_logo = e.target.value
+        }else if(e.target.name === "teamAbbr"){
+            this.state.abbr = e.target.value
+        }
+        this.setState({
+            team_name:this.state.team_name,
+            team_logo:this.state.team_logo,
+            abbr:this.state.abbr
+        });
     }
     render(){
-        const tooltip = (
-                    <Tooltip id="tooltip">Add more players</Tooltip>
-)                       ;
         return(
             <div>
                 <div className="banner create-match">
@@ -21,7 +63,7 @@ class CreateTeam extends Component{
                 </div>
                 <div className="pick-container">
                     <div className="row">
-                        <div className="panel panel-primary col-lg-5 slct">
+                        <div className="panel panel-primary col-lg-6 col-lg-offset-3 slct">
                             <div className="panel-body">
                                 <h2>Fill in your team's detail here</h2>
                             </div>
@@ -29,44 +71,58 @@ class CreateTeam extends Component{
                                 <form>
                                     <div className="form-group">
                                         <label for="exampleInputTeam">Team Name</label>
-                                        <input type="text" className="form-control" id="exampleInputTeam" placeholder="Kickass Name"/>                               
+                                        <input 
+                                            type="text" 
+                                            className="form-control" 
+                                            id="exampleInputTeam" 
+                                            name="teamName"
+                                            onChange={this.txtFieldChange}
+                                            placeholder="Kickass Name"/>                               
                                     </div>
                                     <div className="form-group">
                                         <label for="exampleInputTeamLogo">URL Link to Team Logo</label>
-                                        <input type="text" className="form-control" id="exampleInputTeamLogo" placeholder="URL Link"/>                               
+                                        <input 
+                                            type="text" 
+                                            className="form-control" 
+                                            id="exampleInputTeamLogo"
+                                            name="logoURL"
+                                            onChange={this.txtFieldChange}
+                                            placeholder="URL Link"/>                               
                                     </div>
                                     <div className="form-group">
                                         <label for="exampleInputTeamAbbr">Team Abbreviation (Short form of team name)</label>
-                                        <input type="text" className="form-control" id="exampleInputTeamAbbr" placeholder="AFC"/>                               
+                                        <input 
+                                            type="text" 
+                                            className="form-control" 
+                                            id="exampleInputTeamAbbr" 
+                                            name="teamAbbr"
+                                            onChange={this.txtFieldChange}
+                                            placeholder="AFC"/>                               
                                     </div>
                                 </form>
-                            </div>
-                        </div>
-                        <div className="col-lg-6 col-lg-offset-1 panel panel-danger slct">
-                            <div className="panel-body">
-                                <h2>Players Form</h2>
-                            </div>
-                            <div className="panel-footer">
-                                <form className="form-inline">
-                                    <div className="form-group">
-                                        <label for="exampleInputName2">Name</label>
-                                        <input type="text" className="form-control" id="exampleInputName2" placeholder="Jane Doe"/>
-                                    </div>
-                                    <div className="form-group">
-                                        <label for="squadNumber">Squad Number</label>
-                                        <input type="email" className="form-control" id="squadNumber" placeholder="11"/>
-                                    </div>
-                                    <OverlayTrigger placement="top" overlay={tooltip}>
-                                        <Button bsStyle="default">+</Button>
-                                    </OverlayTrigger>
-                                </form>                               
                             </div>
                         </div>
                     </div>   
                     <div className="row">
                         <div className="col-lg-6 col-lg-offset-3">
                             <h3>All Set? Let's get going!</h3>
-                            <button type="button" className="btn btn-primary btn-lg btn-block">Review Team</button>
+                            <button onClick={this.open} type="button" className="btn btn-primary btn-lg btn-block">Review Team</button>
+                            <Modal show={this.state.showModal} onHide={this.close}>
+                                <Modal.Header closeButton>
+                                    <Modal.Title>Review your roster</Modal.Title>
+                                </Modal.Header>
+                                <Modal.Body>
+                                    <h4>Team Name</h4>
+                                    <p>{this.state.team_name}</p>
+                                    <hr />
+                                    <h4>Team Abbr</h4>
+                                    <p>{this.state.abbr}</p>
+                                </Modal.Body>
+                                <Modal.Footer>
+                                    <Button onClick={this.saveTeam} bsStyle="primary">Submit Team</Button>
+                                    <Button onClick={this.close}>Close</Button>
+                                </Modal.Footer>
+                            </Modal>
                         </div>
                     </div>
                 </div>
