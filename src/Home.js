c3bc586013163e2e5ac9ecda19d22fdd7c035e81
CrimsonPug <ikram.tuan.mustapha@gmail.com>
2016-12-11 19:47:20 -0500

    footer

--- src/Home.js
+++ src/Home.js
@@ -46,6 +46,42 @@ class Home extends Component {
                         <p>Add your friends into the team!</p>
                     </div>
                 </div>
+                <footer className="footer">
+                    <section >
+                        <div className="social">
+                            <ul className="footer-list social-list">
+                                <li>
+                                    <a className="contact-link" href="https://linkedin.com/in/ikram-mustapha-b1210912a" target="_blank">
+                                        <img className="icon-social" src={linked} />
+                                    </a>
+                                </li>
+                                <li>
+                                    <a className="contact-link" href="https://twitter.com/IkramTM" target="_blank">
+                                        <img className="icon-social" src={twitter} />
+                                    </a>
+                                </li>
+                                <li>
+                                    <a className="contact-link" href="https://github.com/CrimsonPug" target="_blank">
+                                        <img className="icon-social" src={gitHub} />
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                        <ul className="footer-list">
+                            <li>
+                                <a className="contact-link" href="https://github.com/CrimsonPug/ultimate-score" target="_blank">
+                                    GitHub Repo
+                                </a>
+                            </li>
+                            <li className="divider">.</li>
+                            <li>
+                                <a className="contact-link" href="mailto:ikram.tuan.mustapha@gmail.com?Subject=Awesome%20project!" target="_top" target="_blank">
+                                    Made by Ikram Mustapha
+                                </a>
+                            </li>
+                        </ul>
+                    </section>
+                </footer>
             </div>           
         )
     }
