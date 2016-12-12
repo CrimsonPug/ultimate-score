b8e5ec2f2c4a45e0433f5a3d948964d43af10cbd
CrimsonPug <ikram.tuan.mustapha@gmail.com>
2016-12-11 20:02:37 -0500

    bug fix

--- src/App.js
+++ src/App.js
@@ -241,8 +241,8 @@ class Register extends Component {
 class Nav extends Component {
    render() {
         return (
-            <div className="nav-list">
-              <ul>
+            <div>
+              <ul className="nav-list">
                     <li><Link to="/">Home</Link></li>
                     <li><Link to="/login">Login</Link></li>
                     <li><Link to="/register">Register</Link></li>             
